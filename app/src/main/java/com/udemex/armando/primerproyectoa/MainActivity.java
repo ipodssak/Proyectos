6f632904c23bb570f2a2f06d0c357e0b35b9a346
ipodssak <ipodssak@live.com.mx>
2016-09-04 14:18:52 -0500

    Proyecto1
    Proyecto 1

---
+++ app/src/main/java/com/udemex/armando/primerproyectoa/MainActivity.java
@@ -0,0 +1,13 @@
+package com.udemex.armando.primerproyectoa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
